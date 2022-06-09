37d70049f011a5b34f1af9b3fe3b428a77483408
borhan azhdari <borhanaz1379@gmail.com>
2022-06-10 03:43:51 +0430

    schema added

---
+++ src/resources/stud/stud.model.js
@@ -0,0 +1,48 @@
+var mongoose = require("mongoose");
+
+var usersSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+  },
+  name: {
+    type: String,
+  },
+  fname: {
+    type: String,
+  },
+  evidence: {
+    type: String,
+  },
+  course_code: {
+    type: Number,
+  },
+  course_name: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  seat_number: {
+    type: Number,
+  },
+  exam_type: {
+    type: String,
+  },
+  course_type: {
+    type: String,
+  },
+  department: {
+    type: String,
+  },
+  class: {
+    type: String,
+  },
+  row: {
+    type: String,
+  }
+});
+
+module.exports = mongoose.model("stud", usersSchema);
