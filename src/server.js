ee3d73f79a736e42164fde09d07159b4640cbd5e
borhan azhdari <borhanaz1379@gmail.com>
2022-06-10 03:43:58 +0430

    updated

--- src/server.js
+++ src/server.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const bot = require('./bot/bot')
-require("dotenv").config();
-
- const app = express();
+const bot = require("./bot/bot");
+const _crawler = require("./crawler/crawler");
+const { default: mongoose } = require("mongoose");
+const app = express();
 
 app.disable("x-powered-by");
 
 app.use(bodyParser.json());
 
-
- const start = () => {
+const start = () => {
   try {
     app.listen(5000, () => {
       console.log(`REST API on http://localhost:${5000}/api`);
+      //connect to db
+      mongoose
+        .connect("mongodb://localhost:27017/PnuNumberDb", {
+          useNewUrlParser: true,
+        })
+        .then(() => console.log("connected to db"))
+        .catch((err) => console.log(err));
+      _crawler();
     });
   } catch (e) {
     console.error(e);
   }
-  bot();
 
+  //bot();
 };
 start();
