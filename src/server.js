a5d03f3064ba3b2e29cd4bf3d9da94d863b5c915
borhan azhdari <borhanaz1379@gmail.com>
2022-06-10 23:09:28 +0430

    updates

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const bot = require("./bot/bot");
 const _crawler = require("./crawler/crawler");
 const { default: mongoose } = require("mongoose");
 const app = express();
-
+const studRoute = require('./resources/stud/stud.router');
 app.disable("x-powered-by");
 
 app.use(bodyParser.json());
-
+app.use(studRoute);
 const start = () => {
   try {
     app.listen(5000, () => {
-      console.log(`REST API on http://localhost:${5000}/api`);
+      console.log(`REST API on http://localhost:${5000}`);
       //connect to db
       mongoose
         .connect("mongodb://localhost:27017/PnuNumberDb", {
@@ -20,12 +20,12 @@ const start = () => {
         })
         .then(() => console.log("connected to db"))
         .catch((err) => console.log(err));
-      _crawler();
+      //_crawler();
     });
   } catch (e) {
     console.error(e);
   }
 
-  //bot();
+  bot();
 };
 start();
