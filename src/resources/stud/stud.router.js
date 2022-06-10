81878d33c74e27e23b29390ca6b58b9432d65a90
borhan azhdari <borhanaz1379@gmail.com>
2022-06-10 23:10:47 +0430

    routes added

---
+++ src/resources/stud/stud.router.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+require("dotenv").config();
+const user = require("./stud.model");
+const crawler = require("../../crawler/crawler");
+const router = Router();
+
+router.get("/updateData", async (req, res) => {
+  let key = req.query.key;
+  if (key == process.env.KEY) crawler();
+  throw "oops wrong place!";
+});
+
+router.get("/api/stud/find/:id", async (req, res) => {
+  let id = req.params.id;
+  const info = await user.find(
+    { id: id },
+    {
+      _id: 0,
+      id: 0,
+    }
+  );
+  // const studvm = new {
+
+  // }
+  //user.find({},)
+  if (info) await res.send(info);
+  else res.status(404).send("موردی یافت نشد");
+  //res.send(info);
+});
+router.get("/api/stud/next/:code/:seat", async (req, res) => {
+  let c_code = req.params.code;
+  let s_number = req.params.seat;
+  let infos = await user
+    .find({ course_code: c_code, seat_number: { $gte: s_number - 2 } })
+    .limit(5);
+  res.send(infos);
+  console.log(infos);
+});
+module.exports = router;
