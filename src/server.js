31d7433a45956e13ee4af3f15e5e61d7c95b657d
borhan azhdari <borhanaz1379@gmail.com>
2022-06-11 19:57:03 +0430

    updated server config

--- src/server.js
+++ src/server.js
@@ -12,14 +12,14 @@ app.use(studRoute);
 const start = () => {
   try {
     const PORT = process.env.PORT || 3000;
-    app.listen(5000, () => {
-      console.log(`REST API on http://localhost:${3000}`);
+    app.listen(PORT, () => {
+      console.log(`run in port: ${PORT}`);
       //connect to db
       mongoose
         .connect(process.env.MONGODB_URI+'/PnuNumberDb' || "mongodb://localhost:27017/PnuNumberDb", {
           useNewUrlParser: true,
         })
-        .then(() => console.log("connected to db"))
+        .then(() => console.log("connected to db: ",process.env.MONGODB_URI))
         .catch((err) => console.log(err));
       //_crawler();
     });
